30ff17c545e5c0216937779c38cec50424723443
FBalduin <30518880+FBalduin@users.noreply.github.com>
2022-09-08 22:52:43 +0200

    Add files via upload

---
+++ Administration.java
@@ -0,0 +1,22 @@
+class Administration	{
+			
+	public static void main(String[] args)	{	
+			
+			// Lege Schueler einmal an
+			User u1 = new User("Felix","Balduin",new Date("14.02.89"));
+			User u2 = new User("Moni","Thor",new Date("15.02.89"));
+			User u3 = new User("Ägar","Keks",new Date("16.02.89"));
+			User u4 = new User("Matt","Eagle",new Date("17.02.89"));
+			
+		    // Gebe einmal beispielhaft die Daten auf der Konsole aus...
+		    u1.printData();
+		    u2.printData();
+		    u3.printData();
+		    u4.printData();
+		    
+		    
+		
+	}
+	
+	
+}
